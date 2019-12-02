d0ec2e456cf4f4ce909b11e7a6f7db9c4db25946
Robert Silva <robert.silva4@fatec.sp.gov.br>
2019-12-02 16:28:17 -0300

    implementação de etidades

--- database/cadastro.sql
+++ database/cadastro.sql
@@ -1,1 +1,30 @@
-﻿
+﻿CREATE DATABASE CADASTRO
+
+	CREATE TABLE TB_TELEFONE(
+		ddd char(2) not null,
+		numero char(9) not null,
+		tipo varchar(12) not null, 
+		constraint PK_Telefone primary key(numero)
+	)
+	CREATE TABLE TB_ENDERECO(
+		idEndereco int not null,
+		rua varchar(120) not null,
+		numero int not null,
+		bairro varchar(120) not null,
+		cidade varchar(120) not null,
+		cep varchar(10) not null,
+		tipo varchar(10) not null,
+		constraint PK_Endereco primary key(idEndereco)
+	)
+	
+	CREATE TABLE TB_CLIENTE(
+		idCliente int not null,
+		nome varchar(120) not null,
+		cpf varchar(13) not null,
+		cnpj varchar(18) not null,
+		endereco int not null,
+		telefone char(9) not null,
+		constraint PK_Cliente primary key(idCliente),
+		constraint FK_Endereco foreign key(endereco) references TB_ENDERECO,
+		constraint FK_Numero foreign key(telefone) references TB_TELEFONE
+	)
---
+++ dot.net.junior/Model.Entidades/Telefone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entidades
+{
+    public class Telefone
+    {
+        public int ddd { get; set }
+        public int telefone { get; set; }
+        public string tipo { get; set; }
+    }
+}
