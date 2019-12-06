e71d5f099474cc8e88b2bc9e008c6f6d2b2f8b68
Robert Silva <robert.silva4@fatec.sp.gov.br>
2019-12-06 05:27:20 -0300

    inserindo acesso  co a tabela telefone

---
+++ dot.net.junior/Model.Infraestrutura/AcessoEntidades/TelefoneAcess.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Model.Entidades;
+using System.Data.SqlClient;
+using Model.Infraestrutura.Acesso_ao_banco;
+using Model.Infraestrutura.Cast;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Infraestrutura.AcessoEntidades
+{
+    public class TelefoneAcess : BaseDao<Telefone>
+    {
+        public TelefoneAcess() { }
+        public TelefoneAcess(acesso acesso) : base(acesso) { }
+
+        public override List<Telefone> CastToObject(SqlDataReader Reader)
+        {
+            List<Telefone> telefones = new List<Telefone>();
+            while (Reader.Read())
+            {
+                telefones.Add(DataCast.CastTelefone(Reader));
+            }
+            return telefones;
+        }
+
+        public Telefone Insert(Telefone telefone)
+        {
+            base.Sql.Append(" INSERT INTO TB_TELEFONE(ddd, numero, tipo) ");
+            base.Sql.Append(" VALUES ('@DDD', '@NUMERO','@TIPO') ");
+
+            base.AddParameter("@DDD", telefone.ddd);
+            base.AddParameter("@NUMERO", telefone.telefone);
+            base.AddParameter("@TIPO", telefone.tipo);
+
+            return telefone;
+        }
+
+        public Telefone Update(Telefone telefone)
+        {
+            base.Sql.Append(" UPDATE TB_TELEFONE SET ");
+            base.Sql.Append(" ddd = @DDD,numero = @NUMERO,tipo = @TIPO) ");
+            base.Sql.Append(" WHERE ID = @ID_TELEFONE ");
+
+            base.AddParameter("@DDD", telefone.ddd);
+            base.AddParameter("@NUMERO", telefone.telefone);
+            base.AddParameter("@TIPO", telefone.tipo);
+
+            base.ExecuteComand();
+
+            return telefone;
+        }
+
+        protected override void SqlBase()
+        {
+            base.Sql.Append(" SELECT ");
+            base.Sql.Append("   ddd AS DDD, ");
+            base.Sql.Append("   numero AS NUMERO,  ");
+            base.Sql.Append("   tipo AS TIPO ");
+            base.Sql.Append(" FROM TB_TELEFONE ");
+        }
+
+        public List<Telefone> Select()
+        {
+            this.SqlBase();
+            using (var Reader = base.ExecuteReader())
+            {
+                return this.CastToObject(Reader);
+            }
+        }
+    }
+}
